24cefcfe4706dc6c9219fcb19bc72892095be2f0
Adam Siergiej <adamsiergiej25@gmail.com>
2026-01-13 15:49:03 -0800

    basic steam_api completed

--- src/steam_api.py
+++ src/steam_api.py
@@ -10,10 +10,11 @@ import math
 import time
 import re
 
-the_skin = " " 
-url = f"https://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name={the_skin}"
+the_skin = "" 
+url = f"https://steamcommunity.com/market/priceoverview/"
 skin_names_url = "https://raw.githubusercontent.com/ByMykel/CSGO-API/main/public/api/en/skins_not_grouped.json"
 parameters = { 
+    "market_hash_name": the_skin,
     "appid": 730,
     "currency" : 20, # code for CAD, USD is 1
     "language" : "english",
@@ -22,9 +23,9 @@ parameters = {
 
 # ------------ Parsing Functions ----------------
 
-
-def fetch_request(url, params=None, headers=None, timeout=10):
+def fetch_request(url, the_skin, params=None, headers=None, timeout=10):
     skin_data = None
+    params.update({"market_hash_name": the_skin})
     response = requests.get(url, params=params, headers=headers, timeout=timeout)
     if response.status_code == 200:
         skin_data = response.json() # raw JSON data
@@ -34,7 +35,7 @@ def fetch_request(url, params=None, headers=None, timeout=10):
 
 def get_all_skin_names(url, params=None, headers=None, timeout=10):
     skin_names = []
-    data = fetch_request(url)
+    data = requests.get(skin_names_url).json()
     for it in data:
         hash_name =  it.get("market_hash_name")
         skin_names.append(hash_name)
@@ -42,5 +43,8 @@ def get_all_skin_names(url, params=None, headers=None, timeout=10):
 
 # ------------ Testing ----------------     
 
-info = fetch_request(url, parameters)
-print(info)
+# skin_array = get_all_skin_names(skin_names_url)
+
+# for skin in skin_array:
+#     info = fetch_request(url,skin,parameters)
+#     print(info)
