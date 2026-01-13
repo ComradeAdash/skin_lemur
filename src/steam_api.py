fda382d493854d9ab9a3d32b9316669d52f348e6
Adam Siergiej <adamsiergiej25@gmail.com>
2026-01-13 15:34:33 -0800

    added a functio to get every skin name

--- src/steam_api.py
+++ src/steam_api.py
@@ -1,42 +1,29 @@
-""""
+"""
 
 A simple script to fetch a single skin's data. 
 
 """
 
-
 import requests
 import urllib.parse
+import math
+import time
+import re
 
-the_skin = "M4A4 | Desolate Space (Factory New)"
-encoded_name = urllib.parse.quote(the_skin, safe='') # 'percent' encodes the skin name for the URL
-
-url = (
-    f"https://steamcommunity.com/market/listings/730/{encoded_name}/render"
-    "?start=0"
-    "&count=10"
-    "&currency=1"
-    "&language=english"
-    "&format=json"
-)
+the_skin = " " 
+url = f"https://steamcommunity.com/market/priceoverview/?appid=730&market_hash_name={the_skin}"
+skin_names_url = "https://raw.githubusercontent.com/ByMykel/CSGO-API/main/public/api/en/skins_not_grouped.json"
+parameters = { 
+    "appid": 730,
+    "currency" : 20, # code for CAD, USD is 1
+    "language" : "english",
+    "format" : "json"
+}
 
 # ------------ Parsing Functions ----------------
 
-"""     
-Fetches data from the specifeid URL
 
-Parameters:
-    url - str
-    params -dict (optional)
-    headers - dict (optional)
-    timeout - int (optional, default=10)
-
-    returns a JSON response from the requested server / url
-
-    *Steam Marrket API returns all the information inside the listinginfo attribute
-"""
-
-def fetch_data(url, params=None, headers=None, timeout=10):
+def fetch_request(url, params=None, headers=None, timeout=10):
     skin_data = None
     response = requests.get(url, params=params, headers=headers, timeout=timeout)
     if response.status_code == 200:
@@ -45,10 +32,15 @@ def fetch_data(url, params=None, headers=None, timeout=10):
     else:
         print(f"Error: Unable to fetch data, status code {response.status_code}")
 
+def get_all_skin_names(url, params=None, headers=None, timeout=10):
+    skin_names = []
+    data = fetch_request(url)
+    for it in data:
+        hash_name =  it.get("market_hash_name")
+        skin_names.append(hash_name)
+    return skin_names
 
-skin_info = fetch_data(url)
-skin_info.get("listinginfo", {})
-for key, value in skin_info["listinginfo"].items():
-    print(f"Listing ID: {key}")
-    print(f"Skin Data: {value}")
-    print("\n")
+# ------------ Testing ----------------     
+
+info = fetch_request(url, parameters)
+print(info)
